f2a8b2eaf7d0b8a9655c1d9552c2bae96cba23a6
Alexandru Ivanescu <ivanescu.alexandru01@gmail.com>
2023-01-26 01:56:28 +0200

    continue methods and properties of array

--- Array.js
+++ Array.js
@@ -16,14 +16,14 @@ let price = [
     1925.33,
     25.01,
     1030,
-    1651,
+    1655,
     11000
 ];
 
 console.log(price);
 
 
-let lenght = lenghtArray()
+let lenght = lenghtArray();
 
 function lenghtArray() {
 
@@ -44,14 +44,56 @@ function lenghtArray() {
 
 for (let i = 0; i < metal.length; i++) {
 
-
     console.log('the length of array is' + ' ' + metal.length);
 }
 
 for (let i = 0; i < price.length; i++) {
 
-
     console.log('the length of array is' + ' ' + price.length);
 }
 
+const metalPriceArray = metal.concat(price);
 
+console.log(metalPriceArray)
+
+for (let i = 0; i < metalPriceArray.length; i++) {
+
+
+    if (metalPriceArray[i] === "Gold") {
+
+        metalPriceArray[i] = metalPriceArray[i] + ' = ' + metalPriceArray[5] + '$';
+
+    }
+
+    if (metalPriceArray[i] === "Silver") {
+
+        metalPriceArray[i] = metalPriceArray[i] + ' = ' + metalPriceArray[6] + '$';
+
+    }
+
+    if (metalPriceArray[i] === "Platinum") {
+
+        metalPriceArray[i] = metalPriceArray[i] + ' = ' + metalPriceArray[7] + '$';
+
+    }
+
+    if (metalPriceArray[i] === "Palladium") {
+
+        metalPriceArray[i] = metalPriceArray[i] + ' = ' + metalPriceArray[8] + '$';
+
+    }
+
+    if (metalPriceArray[i] === "Rhodium") {
+
+        metalPriceArray[i] = metalPriceArray[i] + ' = ' + metalPriceArray[9] + '$';
+
+    }
+
+    if (metalPriceArray[i] === metalPriceArray[5]) {
+
+        metalPriceArray[i] = metalPriceArray.splice(0, 5);
+
+        console.log(metalPriceArray[i])
+    }
+
+}
